716644752e9b0ee1ddf3618340dcd06129ec8a2e
sbarciela <barcielasebastian@gmail.com>
2021-01-26 23:17:47 -0300

    agregando rutas en app.js,estructura basica de los documentos html y links de los head de cada documento

--- app.js
+++ app.js
@@ -9,3 +9,19 @@ app.use(express.static(publicPath));
 app.listen(3000, ()=>{
     console.log("servidor corriendo en puerto 3000");
 });
+
+app.get("/" , (req,res)=>{
+    res.sendFile(path.resolve("./views/index.html"))
+});
+app.get("/views/productCart" , (req,res)=>{
+    res.sendFile(path.resolve("./views/productCart.html"))
+});
+app.get("/views/productDetail" , (req,res)=>{
+    res.sendFile(path.resolve("./views/productDetail.html"))
+});
+app.get("/views/login" , (req,res)=>{
+    res.sendFile(path.resolve("./views/login.html"))
+});
+app.get("/views/register" , (req,res)=>{
+    res.sendFile(path.resolve("./views/register.html"))
+});
