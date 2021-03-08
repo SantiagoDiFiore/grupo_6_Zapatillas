0546c906b36612fd1e196e16f2f737071e1eb835
ramatc <ramitanquias@hotmail.com>
2021-03-08 19:23:53 -0300

    Modifico el archivo de entrada app

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
+// ************ Require's ************
 const express= require("express");
-const app= express();
 const path=require("path");
 const methodOverride=require("method-override");
 
+// ************ express() - (don't touch) ************
+const app = express();
+
+// ************ Middlewares - (don't touch) ************
+app.use(express.static("../public"))
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
-app.use(methodOverride("_method"));
+app.use(methodOverride("_method")); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
 
+// ************ Template Engine - (don't touch) ************
+app.set("view engine", "ejs");
 
-app.use(express.static("../public"))
-
-
+// ************ WRITE YOUR CODE FROM HERE ************
 app.listen(3000, ()=>{
     console.log("Servidor corriendo en puerto 3000");
 });
 
-
-//implementación de las rutas
+// ************ Route System require and use() ************
 const rutasMain= require("./routes/main");
 app.use("/" , rutasMain);
 
@@ -29,9 +33,25 @@ app.use("/users" , rutasUsers);
 const rutasMarcas= require("./routes/marcas");
 app.use("/marcas" , rutasMarcas);
 
-//definiendo el motor de plantillas
+// ************ DON'T TOUCH FROM HERE ************
+// ************ catch 404 and forward to error handler ************
+app.use((req, res, next) => next(createError(404)));
 
-app.set("view engine", "ejs");
+// ************ error handler ************
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.path = req.path;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// ************ exports app - dont'touch ************
+module.exports = app;
+
 
 
 
