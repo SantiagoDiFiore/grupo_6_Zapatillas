1ef71be994a9e65da4523ac612550e2bae5b80d4
sbarciela <barcielasebastian@gmail.com>
2021-04-28 15:40:27 -0300

    actualizando middleware de cookie y session

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -2,15 +2,22 @@
 //TAMBIEN LO USAREMOS PARA GUARDAR LAS COOKIES y PASAR AL USUARIO A SESION
 const User=require("../models/User");//requerimiento del modelo
 
+const db = require('../database/models');
+const Users = db.User;
+const { Op } = require("sequelize");
 
-const userLoggedMiddleware=function(req, res, next){
+
+const userLoggedMiddleware= async function(req, res, next){
 res.locals.isLogged=false;//seteamos una variable que se puede acceder desde cualquier vista, por defecto en false
 
 //guardamos en una variable el mail de la cookie seteada previamente
 let emailInCookie= req.cookies.userEmail;
 
 //usamos el metodo del modelo para buscar al usuario que coincide con el de la cookie
-let userFromCookie= User.findByField("email" , emailInCookie);
+//let userFromCookie= User.findByField("email" , emailInCookie);
+let userFromCookie= await Users.findOne({where:{
+   email: {[Op.like] : emailInCookie}
+}});
 
 //si encomtramos al usuario lo pasamos como valor del usuario logueado
 if(userFromCookie){
