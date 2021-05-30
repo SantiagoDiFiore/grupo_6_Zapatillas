c5d63a8b512c50a2f0609847452121ebd9a7f07b
sbarciela <barcielasebastian@gmail.com>
2021-05-30 00:50:50 -0300

    alert de talles del product detail

--- public/js/productCart.js
+++ public/js/productCart.js
@@ -5,7 +5,7 @@ window.onload = function(){
     let modalSuscripcion = document.querySelector("#suscripcion");
     let botonCerrarSuscripcion = document.querySelector("#cerrarSuscripcion");
 
-    buyButton.addEventListener("click",function(event){
+    /*buyButton.addEventListener("click",function(event){
         function timeOut(){
          for( let i = 0; i < size.length; i++ ) {
             if (!(size[i].checked)) {
@@ -15,5 +15,25 @@ window.onload = function(){
             });
         }}}
         setTimeout(timeOut, 100)
+    })*/
+
+   
+ 
+
+    buyButton.addEventListener("click",function(e){
+        let talles=Array.from(size)//convierte nodelist a array
+
+        //el metodo every evalua que todos los objetos del array cumplan con con condicion
+        let tallesVacios=talles.every(unTalle=>!unTalle.checked)//codicion:no debe estar checked
+        //si no hay talles chequeados
+        if(tallesVacios===true){
+                modalSuscripcion.style.display = "block";
+                botonCerrarSuscripcion.addEventListener("click",function(){
+                modalSuscripcion.style.display = "none"
+                })
+            }else{
+                form.submit()
+                }
     })
+
 }
